d6588498e246cacc7bbb9f794311ad4403b9bd6f
Elena Pronichkina <prolena39@mail.ru>
2023-06-04 22:48:15 +0300

    add utils

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+def read_json():
+    """
+    читает файл json и возвращает его содержимое
+    return: данные, список словарей
+    """
+    pass
+
+def sort_data(data):
+    """
+    сортирует данные по статусу операции
+    :param data: исходные данные
+    :return: отсортированные данные
+    """
+    pass
+
+def mask_card_number(card_number):
+    """
+    маскирует цифры номера карты на *
+    :param card_number: номер карты
+    :return: маскированная строка
+    """
+    pass
+
+def mask_account_number(account_number):
+    """маскирует цифры номера счета на *
+    :param account_number: номер счета
+    :return: маскированная строка"""
+    pass
+
