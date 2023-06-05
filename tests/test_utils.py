affc9b3457b2da139f2be82fd6c2c93e19ada878
Elena Pronichkina <prolena39@mail.ru>
2023-06-05 22:44:12 +0300

    add test and fixtures for read_json

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 from src.utils import read_json
 
-def test_read_json():
-    assert read_json() == []
+
+def test_read_json(path_to_test_json, expected_result_read_json_test):
+    assert read_json(path_to_test_json) == expected_result_read_json_test
