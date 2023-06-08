b2b00bfca0c98d6bc1e0b298a176ee36fb5a949b
Elena Pronichkina <prolena39@mail.ru>
2023-06-08 22:43:30 +0300

    add test and utils for mask_card_number and mask_account_number

--- src/utils.py
+++ src/utils.py
@@ -27,11 +27,55 @@ def mask_card_number(card_number):
     :param card_number: номер карты
     :return: маскированная строка
     """
-    pass
+    for operation in data:
+        card_number = operation['from']
+        mask_card = card_number[0:14] + '*' * len(card_number[15:-4]) + card_number[-4:]
+        return mask_card[:12], mask_card[13:17], mask_card[-9:-5], mask_card[-4:]
+#        print(mask_card[:12], mask_card[13:17], mask_card[-9:-5], mask_card[-4:])
+# "from": "Maestro 1596837868705199"
+
 
 def mask_account_number(account_number):
-    """маскирует цифры номера счета на *
+    """
+    маскирует цифры номера счета на *
     :param account_number: номер счета
-    :return: маскированная строка"""
-    pass
-
+    :return: маскированная строка
+    """
+    for operation in data:
+        account_number = operation['to']
+        return account_number[0:5] + '*' * len(account_number[6:-4]) + account_number[-4:]
+#       print(account_number[0:5] + '*' * len(account_number[6:-4]) + account_number[-4:])
+# "to": "Счет 64686473678894779589"
+#mask_account_number()
+#
+# def modify_date(date):
+#      """меняет формат даты
+#      :param date: дата операции в исходном формате
+#      :return: дата в формате ДД.ММ.ГГГГ (14.10.2018)
+#     """
+#
+#      # dt_string = "2019-08-26T10:50:58.294041"
+#      # dt_str = dt_string[0:10]
+#      # # dt_str_reversed = ''.join(reversed(dt_str))
+#      # dt_modify = datetime.strptime(dt_str, "%d %m %Y")
+#
+# #     for operation in data:
+#      #     date = operation['date']
+# #           modify_date = date.strftime('%d.%m.%Y')
+# #           print (modify_date)
+# #     return modify_date
+#     pass
+#
+# def sort_data_by_date(data):
+#     '''
+#     сортирует данные по дате операции
+#     :param date: дата операции
+#     :return: отсортированные данные'''
+# #     data.sort(key=lambda x: x['date'], reversed=True)
+# #     return data
+# #     sorted_data_for_date = []
+# #     for operation in data:
+# #         ...
+# #         return sorted_data_for_date
+#     pass
+#
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
-from src.utils import read_json, sort_data
+import pytest
+
+from src.utils import read_json, sort_data, modify_date, mask_card_number, mask_account_number
 
 
 def test_read_json(path_to_test_json, expected_result_read_json_test):
@@ -7,3 +9,13 @@ def test_read_json(path_to_test_json, expected_result_read_json_test):
 
 def test_sort_data(data_for_sort, expected_result_for_sort):
     assert sort_data (data_for_sort) == expected_result_for_sort
+
+
+def test_modify_date(date_for_modify, expected_result_for_date):
+    assert modify_date(date_for_modify) == expected_result_for_date
+
+def test_mask_card_number():
+    assert mask_card_number("Maestro 1596837868705199") == "Maestro 1596 83** **** 5199"
+
+def test_mask_account_number():
+    assert mask_account_number("Счет 64686473678894779589") == "Счет ***************9589"
