891e236ec982e57f966f3fe7d774eb5e91d10471
Elena Pronichkina <prolena39@mail.ru>
2023-06-14 21:43:35 +0300

    add main

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
-from src.utils import read_json, sort_data, sort_data_by_date, mask_card_number, mask_account_number
+
+from src.utils import read_json, sort_data, sort_data_by_date, print_message_to_user
 
 if __name__ == '__main__':
-    #прочитать данные
-    read_json()
 
-    #отсортировать данные по статусу
-    sort_data()
+    raw_data = read_json('src/operations.json')
 
-    # отсортированные по статусу данные (sorted_data []) отсортировать по дате
-    sort_data_by_date()
+    sorted_data = sort_data(raw_data)
 
-    #взять последние 5 операций:
-    latest_operations = sort_data_by_date [-5:-1]
+    date_sorted_data = sort_data_by_date(sorted_data)
 
-    for operations in latest_operations:
-        date = operation["date"]
-        description = operation["description"]
-        card_number = operation["from"]
-        account_number = operation["to"]
-        amount = operation["operationAmount"]["amount"]
-        currency = operation["operationAmount"]["currency"]["name"]
+    latest_operations = date_sorted_data[:5]
 
-        #привести дату к формату ДД.ММ.ГГГГ
-        date_for_print = modify_date(date)
-
-        #маскировка номера карты (откуда)
-        operation_from = mask_card_number(card_number)
-
-        #маскировка номера счета (куда)
-        operation_to = mask_account_number(account_number)
-
-#    print(< дата перевода > < описание перевода > < откуда > -> < куда > < сумма перевода > < валюта > "\n")
-        print(f"{date_for_print}, {description}, {operation_from}, '->', {operation_to}, {amount}, {currency} \n")
+    for message in print_message_to_user(latest_operations):
+        print(message, end='\n\n')
 
