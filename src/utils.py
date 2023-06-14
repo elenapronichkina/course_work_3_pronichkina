3bb622e8d52a50516f32a28a6c360f8e5031fc3b
Elena Pronichkina <prolena39@mail.ru>
2023-06-14 21:35:02 +0300

    correct functions and add docstrings

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import json
-import datetime
+
 def read_json(file_path):
     """
     читает файл json и возвращает его содержимое
@@ -17,7 +17,7 @@ def sort_data(data: list[dict]) -> list[dict]:
     """
     sorted_data = []
     for operation in data:
-        if operation['state'] == 'EXECUTED':
+        if operation.get('state') == 'EXECUTED':
             sorted_data.append(operation)
 
     return sorted_data
@@ -28,12 +28,7 @@ def mask_card_number(card_number):
     :param card_number: номер карты
     :return: маскированная строка
     """
-    for operation in data:
-        card_number = operation['from']
-        mask_card = card_number[0:14] + '*' * len(card_number[15:-4]) + card_number[-4:]
-        return mask_card[:12], mask_card[13:17], mask_card[-9:-5], mask_card[-4:]
-#        print(mask_card[:12], mask_card[13:17], mask_card[-9:-5], mask_card[-4:])
-# "from": "Maestro 1596837868705199"
+    return f'{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}'
 
 
 def mask_account_number(account_number):
@@ -42,42 +37,61 @@ def mask_account_number(account_number):
     :param account_number: номер счета
     :return: маскированная строка
     """
-    for operation in data:
-        account_number = operation['to']
-        return account_number[0:5] + '*' * len(account_number[6:-4]) + account_number[-4:]
-#       print(account_number[0:5] + '*' * len(account_number[6:-4]) + account_number[-4:])
-# "to": "Счет 64686473678894779589"
-#mask_account_number()
-#
-# def modify_date(date):
-#      """меняет формат даты
-#      :param date: дата операции в исходном формате
-#      :return: дата в формате ДД.ММ.ГГГГ (14.10.2018)
-#     """
-#
-#      # dt_string = "2019-08-26T10:50:58.294041"
-#      # dt_str = dt_string[0:10]
-#      # # dt_str_reversed = ''.join(reversed(dt_str))
-#      # dt_modify = datetime.strptime(dt_str, "%d %m %Y")
-#
-# #     for operation in data:
-#      #     date = operation['date']
-# #           modify_date = date.strftime('%d.%m.%Y')
-# #           print (modify_date)
-# #     return modify_date
-#     pass
-#
-# def sort_data_by_date(data):
-#     '''
-#     сортирует данные по дате операции
-#     :param date: дата операции
-#     :return: отсортированные данные'''
-#      data.sort(key=lambda x: x['date'], reversed=True)
-#      return data
-#      sorted_data_for_date = []
-#       for operation in data:
-#         #operation['date']
-#         sorted_data_for_date.append(operation)
-#         return sorted_data_for_date
-#     pass
-#
+    return f"**{account_number[-4:]}"
+
+def modify_date(date):
+    """
+    меняет формат даты
+    :param date: дата операции в исходном формате
+    :return: дата в формате ДД.ММ.ГГГГ (14.10.2018)
+    """
+    new_date = date[0:10]
+    list_date = new_date.split("-")
+    return f"{list_date[2]}.{list_date[1]}.{list_date[0]}"
+
+def sort_data_by_date(data):
+    '''
+    сортирует данные по дате операции
+    :param data: данные
+    :return: отсортированные данные
+    '''
+    data.sort(key=lambda x: x['date'], reverse=True)
+    return data
+
+def print_message_to_user(latest_operations):
+     """
+     выводит на экран список из 5 последних выполненных клиентом операций в формате:
+    <дата перевода> <описание перевода>
+    <откуда> -> <куда>
+    <сумма перевода> <валюта>
+     :param latest_operations:
+     :return:
+     """
+    messages = []
+    for operation in latest_operations:
+
+        date = operation["date"]
+        description = operation["description"]
+        card_number = operation.get("from")
+        account_number = operation["to"]
+        amount = operation["operationAmount"]["amount"]
+        currency = operation["operationAmount"]["currency"]["name"]
+
+        date_for_print = modify_date(date)
+
+        if card_number is None:
+            operation_from = 'Информация не указана'
+            card_name = ''
+        elif card_number.split()[0] == 'Счет':
+            operation_from = mask_account_number(card_number[-20:])
+            card_name = 'Счет'
+        else:
+            operation_from = mask_card_number(card_number[-16:])
+            card_name = card_number[:-17]
+
+        operation_to = mask_account_number(account_number[-20:])
+
+        messages.append(
+            f"{date_for_print} {description}\n{card_name} {operation_from} -> Счет {operation_to}\n{amount} {currency}")
+
+    return messages
